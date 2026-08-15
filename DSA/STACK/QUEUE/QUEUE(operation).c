9097a8e784e1e0ac6322e542cf59cbe833451f4f
Aryan Rawat <aryanawat2007@gmail.com>
2026-08-15 21:13:16 +0530

    Update QUEUE(operation).c

--- DSA/STACK/QUEUE/QUEUE(operation).c
+++ DSA/STACK/QUEUE/QUEUE(operation).c
@@ -1,3 +1,12 @@
+Design an algorithm and a program to implement queue using array. The program should
+implement following queue operations:
+a) Create() - create a queue
+b) EnQueue(k) - insert an element k into the queue
+c) DeQueue() - delete an element from the queue
+d) IsEmpty() - check if queue is empty or not
+e) Size() - finds the size of the queue
+
+
 #include<stdio.h>
 int queue[100],front=-1,rear=-1;
 void enqueue(int x){
