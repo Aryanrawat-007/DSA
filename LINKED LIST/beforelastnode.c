b36f4df1a6a69fcef460a4203c70640ff98c4948
Aryan Rawat <aryanawat2007@gmail.com>
2026-08-26 20:54:30 +0530

    Add files via upload

---
+++ LINKED LIST/beforelastnode.c
@@ -0,0 +1,49 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct list {
+    int data;
+    struct list *next;
+} sl;
+
+int main() {
+    sl *p, *new, *head, *t;
+    int a,b,c,d;
+    head = malloc(sizeof(sl));
+    printf("ENTER FIRST VALUE TO INSERT \n");
+    scanf("%d",&a);
+    head->data = a;
+
+    head->next = malloc(sizeof(sl));
+    printf("ENTER SECOND VALUE TO INSERT \n");
+    scanf("%d",&b);
+    head->next->data = b;
+
+    head->next->next = malloc(sizeof(sl));
+    printf("ENTER THIRD VALUE TO INSERT \n");
+    scanf("%d",&c);
+    head->next->next->data = c;
+    head->next->next->next = NULL;
+
+    new = malloc(sizeof(sl));
+    printf("ENTER VALUE TO INSERT BEFORE LAST NODE\n");
+    scanf("%d",&d);
+    new->data = d;
+
+    p = head;
+
+    while(p->next->next != NULL) {
+        p = p->next;
+    }
+
+    new->next = p->next;
+    p->next = new;
+
+    t = head;
+    while(t != NULL) {
+        printf("%d ", t->data);
+        t = t->next;
+    }
+
+    return 0;
+}
