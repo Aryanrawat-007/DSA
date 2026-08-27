1e3a5f49688d0e2889d67ebe2d04acace7a37253
Aryan Rawat <aryanawat2007@gmail.com>
2026-08-27 20:03:16 +0530

    Add files via upload

---
+++ LINKED LIST/deletealternatenode.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct list{
+    int data;
+    struct list*next;
+}sl;
+int main(){
+    sl*p,*head=NULL,*del,*temp;
+    int i,n,x;
+    printf("ENTER NUMBER");
+    scanf("%d",&n);
+    for(i=0;i<n;i++){
+        printf("ENTER DATA");
+        scanf("%d",&x);
+        p=malloc(sizeof(sl));
+        p->data=x;
+        p->next=NULL;
+        if(p==NULL){
+            printf("invalid");
+            return 1;
+        }
+        if(head==NULL){
+        head=p;
+        }
+        else{
+            temp=head;
+            while(temp->next!=NULL){
+                temp=temp->next;
+            }
+            temp->next=p;
+        }
+        
+    }
+    temp=head;
+    while(temp!=NULL &&temp->next!=NULL){
+        del=temp->next;
+        temp->next=del->next;
+        free(del);
+        temp=temp->next;
+    }
+    temp = head;
+
+    printf("After deleting alternate nodes: ");
+
+    while (temp != NULL) {
+        printf("%d -> ", temp->data);
+        temp = temp->next;
+    }
+
+    printf("NULL\n");
+
+    return 0;
+}
